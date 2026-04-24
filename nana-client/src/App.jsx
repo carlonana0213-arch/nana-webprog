76a608f675d5fda9ff04933afff2fadd3f1cf182
Carlo Nana <carlonana0213@gmail.com>
2026-04-24 20:56:44 +0800

    lab-act1

---
+++ nana-client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+  
+          <div className='App'>
+          
+          <header className="App-header">
+            <h1>Welcom to My React App! </h1>
+            <p>
+              Name: Carlo Raphael Nana <br/>
+              Email: carlonana0213@gmail.com <br/>
+              Github: https://github.com/carlonana0213-arch
+            </p>
+          </header>
+          </div>
+  )
+}
+
+export default App
